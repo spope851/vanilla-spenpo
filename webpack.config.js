a7ec0a5cd7caa7f8fbb849a91986c19cd2eb6015
Spencer Pope <spenpo@spenpo.com>
2024-12-07 13:02:44 +0800

    configure webpack dev environment and node prod environment

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,62 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-module.exports = {
-  entry: './src/js/index.js', // Main file
-  output: {
-    filename: 'js/index.js',
-    path: path.resolve(__dirname, 'dist'), // Output directory
-  },
-  mode: 'development', // Change to 'production' for optimizations
-  module: {
-    rules: [
-      {
-        test: /\.css$/, // Process CSS
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.js$/, // Transpile JS
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
+module.exports = (_env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: [
+            // In production, use MiniCssExtractPlugin
+            isProduction && MiniCssExtractPlugin.loader,
+            // In development, you might use 'style-loader' instead
+            !isProduction && 'style-loader',
+            'css-loader'
+          ]
+        },
+        {
+          test: /\.js$/, // Transpile JS
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
           },
         },
-      },
-    ],
-  },
-  plugins: [
-    new CopyWebpackPlugin({
-      patterns: [
-        { from: 'src/*.html', to: '[name][ext]' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
       ],
-    }),
-  ],
-};
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        favicon: './public/favicon.ico'
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'styles.css' // Output CSS file name
+      })
+    ],
+    // Development server configuration
+    devServer: {
+      // In development, serve from source
+      static: path.join(__dirname, 'src'),
+      // In production, serve from dist
+      ...(isProduction && { 
+        static: path.join(__dirname, 'dist') 
+      }),
+      hot: true,
+      open: true
+    },
+  }
+}
 
